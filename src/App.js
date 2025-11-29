f21b796055ff4493d593b4c39ab9d4de1b75059b
Rohit <rohitghadge348@gmail.com>
2025-11-30 00:29:14 +0530

    completed by using redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Fragment } from 'react/jsx-runtime';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from "./components/Auth";
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
+
 
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  // console.log(isAuthenticated)
   return (
-    <Counter />
+    <Fragment>
+      <Header/>
+      {isAuthenticated && <UserProfile/>}
+      {!isAuthenticated && <Auth/>} 
+      <Counter />
+    </Fragment>
   );
 }
 
